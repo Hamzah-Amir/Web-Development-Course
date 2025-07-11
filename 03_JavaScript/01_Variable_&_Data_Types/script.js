18e2347217ee9b99fd5c1596786acf140bf72ce7
Hamza <hamzaamir7651@gmail.com>
2025-07-11 12:42:15 +0500

    Data types and Varaibles in JavaScript

---
+++ 03_JavaScript/01_Variable_&_Data_Types/script.js
@@ -0,0 +1,33 @@
+console.log("Introduction of JavaScript")
+
+var a = 10
+var b = 25
+var c = "Hamza"
+console.log(a*b)
+
+console.log(typeof b, ",", typeof c)
+
+const a1 = 4 
+// const a1 = 10
+// We cannot assign another value to same variable 
+console.log(a1)
+
+let d = 45
+let e = "Hamza Amir"
+let f = 3.45
+let g = true
+let h = null
+let i = undefined
+
+console.log(d,e,f,g,h,i)
+console.log(typeof d,typeof e,typeof f,typeof g,typeof h,typeof i)
+
+const k = {
+    name : "Hamza",
+    job: "Web Developer",
+    language: "HTML, CSS, JavaScript",
+    is_working: true
+}
+console.log(k)
+
+let m = 25
