0c64934002718e431f4423a9e781a3fbba14a2e6
Hamza <hamzaamir7651@gmail.com>
2025-07-16 16:59:39 +0500

    If Else Ladder

--- 03_JavaScript/02_If_Else/index.js
+++ 03_JavaScript/02_If_Else/index.js
@@ -1,12 +1,21 @@
 console.log("Conditional Tutorial")
 
-var age = 45;
+let age = 18;
+let grace = 2;
+console.log(age =+ grace)
+
 if(age > 18){
-    console.log("You can Drive")
+    console.log("You can Drive");
 }
 
 else{
-    console.log("You cannot drive")
+    console.log("You cannot drive");
 }
+console.log(age);
 
-console.log(age)
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age ** grace)
+console.log(age % grace)
