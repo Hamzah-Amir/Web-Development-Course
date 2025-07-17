a2dce09049c70754dc968289c5daa937b43a5221
Hamza <hamzaamir7651@gmail.com>
2025-07-17 20:58:39 +0500

    Exercise : Calculatethe factorial

--- 04_Projects/09_Caluculate_The_Factorial/index.js
+++
@@ -1,9 +0,0 @@
-function factorial(a) {
-        let result = 1
-        for (let i = 2; i<=a ;i++) {
-            result += i;            
-        }
-        return result
-}
-
-console.log(factorial(3))
