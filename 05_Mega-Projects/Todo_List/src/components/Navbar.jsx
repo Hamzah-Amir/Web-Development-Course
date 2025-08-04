0b12d7bd18f74c2274be943754bfbf6107a8d8a5
Hamza <hamzaamir7651@gmail.com>
2025-08-04 09:39:10 +0500

    Navbar

--- 05_Mega-Projects/Todo_List/src/components/Navbar.jsx
+++ 05_Mega-Projects/Todo_List/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="flex p-5 justify-between text-white mx-auto bg-slate-500">
         <div className="logo text-[20px]"><span className='font-bold'>DailyDo</span></div>
-        <ul className="flex gap-14  text-[20px]">
+        <ul className="flex gap-14 text-[20px]">
             <li className='cursor-pointer hover:font-bold'>Home</li>
             <li className='cursor-pointer hover:font-bold'>Your Tasks</li>
         </ul>
